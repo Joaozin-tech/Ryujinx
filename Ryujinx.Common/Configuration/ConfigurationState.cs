83a599ae225f52e9b55893f613a028230dcd44c4
Xpl0itR <xpl0itr@outlook.com>
2020-05-07 19:33:48 +0100

    nit
    Co-authored-by: Ac_K <Acoustik666@gmail.com>

--- Ryujinx.Common/Configuration/ConfigurationState.cs
+++ Ryujinx.Common/Configuration/ConfigurationState.cs
@@ -435,7 +435,7 @@ namespace Ryujinx.Configuration
             Ui.CustomThemePath.Value               = "";
             Hid.EnableKeyboard.Value               = false;
             
-            Hid.Hotkeys.Value     = new KeyboardHotkeys
+            Hid.Hotkeys.Value = new KeyboardHotkeys
             {
                 ToggleVsync = Key.Tab
             };
